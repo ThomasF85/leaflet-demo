87fbf785a7c222997cc4ecd2cb6e97b909cdd2fa
ThomasF85 <thomas.foeldi@gmail.com>
2023-01-30 12:45:55 +0100

    add custom markers

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,5 +1,6 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import styled from "styled-components";
+import * as L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
@@ -11,6 +12,17 @@ const StyledMapContainer = styled(MapContainer)`
   margin: 0 auto;
 `;
 
+const goldIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
 export default function Map({ markers }) {
   return (
     <StyledMapContainer center={[50.123, 8.234]} zoom={12} scrollWheelZoom>
@@ -20,7 +32,11 @@ export default function Map({ markers }) {
       />
       {markers.map((marker) => {
         return (
-          <Marker key={marker.id} position={[marker.lat, marker.long]}>
+          <Marker
+            key={marker.id}
+            position={[marker.lat, marker.long]}
+            icon={goldIcon}
+          >
             <Popup>
               <h2>{marker.name}</h2>
               <p>This is a very interesting spot</p>
