f48624b311ccae221907d3091f00c9300bc7e3e5
ThomasF85 <thomas.foeldi@gmail.com>
2023-01-30 12:33:38 +0100

    add markers

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,6 +1,8 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import styled from "styled-components";
 import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
 
 const StyledMapContainer = styled(MapContainer)`
   height: 30rem;
@@ -8,13 +10,23 @@ const StyledMapContainer = styled(MapContainer)`
   margin: 0 auto;
 `;
 
-export default function Map() {
+export default function Map({ markers }) {
   return (
     <StyledMapContainer center={[50.123, 8.234]} zoom={12} scrollWheelZoom>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {markers.map((marker) => {
+        return (
+          <Marker key={marker.id} position={[marker.lat, marker.long]}>
+            <Popup>
+              <h2>{marker.name}</h2>
+              <p>This is a very interesting spot</p>
+            </Popup>
+          </Marker>
+        );
+      })}
     </StyledMapContainer>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,26 @@ import dynamic from "next/dynamic";
 
 const Map = dynamic(() => import("../components/Map"), { ssr: false });
 
+const markers = [
+  {
+    id: "7c5qktU8",
+    name: "Roßmarkt Frankfurt",
+    lat: 50.1123904,
+    long: 8.6753527,
+  },
+  {
+    id: "2F11t82g",
+    name: "Currywurst Wagen Hamburg",
+    lat: 53.5450717,
+    long: 10.0267705,
+  },
+];
+
 export default function HomePage() {
   return (
     <>
       <h1>Hello from Next.js</h1>;
-      <Map />
+      <Map markers={markers} />
     </>
   );
 }
